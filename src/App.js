f45cd66f9c5d750e956d2733535d94bd7c241c92
gleekzorp <gleek.dan@gmail.com>
2019-07-14 13:06:53 -0600

    Took away useless returns and console logs

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ function App() {
     let hex = ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     let randomHex = ['#']
     for (let i = 0; i < 6; i++) {
-      let randomNumber = Math.floor(Math.random() * hex.length)
-      randomHex.push(hex[randomNumber])
+      randomHex.push(hex[Math.floor(Math.random() * hex.length)])
     }
-    console.log(randomHex.join(''))
     document.body.style.backgroundColor = randomHex.join('')
     document.querySelector('.hexText').innerHTML = randomHex.join('')
-    // return randomHex.join('')
   }
 
   return (
     <div className="App">
       <button onClick={randomHexGenerator}>CLICK ME</button>
-      <div className='hexText'>
-
-      </div>
+      <div className='hexText'></div>
     </div>
   );
 }
