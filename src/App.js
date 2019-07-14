e918ddbff1dbac5fbd2b3d2a4a32f04ac81bddf7
gleekzorp <gleek.dan@gmail.com>
2019-07-14 13:01:32 -0600

    Functionality works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 function App() {
-  function hello() {
-    console.log('hello')
+  function randomHexGenerator() {
+    let hex = ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let randomHex = ['#']
+    for (let i = 0; i < 6; i++) {
+      let randomNumber = Math.floor(Math.random() * hex.length)
+      randomHex.push(hex[randomNumber])
+    }
+    console.log(randomHex.join(''))
+    document.body.style.backgroundColor = randomHex.join('')
+    document.querySelector('.hexText').innerHTML = randomHex.join('')
+    // return randomHex.join('')
   }
+
   return (
     <div className="App">
-      <button onClick={hello}>CLICK ME</button>
+      <button onClick={randomHexGenerator}>CLICK ME</button>
+      <div className='hexText'>
+
+      </div>
     </div>
   );
 }
